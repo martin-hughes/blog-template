877c79a89900296eba9b38083ee2315b93153c47
Martin Hughes <martin-hughes@users.noreply.github.com>
2025-01-18 22:11:20 +0000

    Switch to tailwind typography for markdown
    This has already made it easier to get the styles of the post content
    correct.

--- src/app/_components/post-body.tsx
+++ src/app/_components/post-body.tsx
@@ -1,11 +1,21 @@
-import markdownStyles from './markdown-styles.module.css'
-
 type Props = {
   content: string
 }
 
 export function PostBody({content}: Props) {
   // We don't get to choose that __html is named against the convention
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  return <div className={markdownStyles['markdown']} dangerouslySetInnerHTML={{__html: content}} />
+  return (
+    <div
+      className='prose dark:prose-invert
+      prose-blockquote:bg-sky-100
+      dark:prose-blockquote:bg-sky-950
+      prose-blockquote:border-l-sky-200
+      dark:prose-blockquote:border-l-sky-700
+      prose-h1:font-bold prose-h1:text-xl
+    prose-a:text-blue-600
+      prose-p:text-justify prose-img:rounded-xl
+      prose-headings:underline' // eslint-disable-next-line @typescript-eslint/naming-convention
+      dangerouslySetInnerHTML={{__html: content}}
+    />
+  )
 }
